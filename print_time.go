4823a896a0cebb096f47c78a386797f56d0f8508
Balkov Arthur <balkov_arthur@mail.ru>
2019-12-10 15:30:11 +0300

    Err handling added

--- print_time.go
+++ print_time.go
@@ -2,15 +2,16 @@ package print_time
 
 import (
 	"fmt"
-
+	"log"
 	"github.com/beevik/ntp"
 )
 
 func PrintDt() bool {
 	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err == nil {
-		fmt.Printf("time now is %s\n", time)
+		fmt.Printf("Time now is %s\n", time)
 	} else {
+		log.Println("Time displaying error: %s\n", err)
 		return false
 	}
 	return true
